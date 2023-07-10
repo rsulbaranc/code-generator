771393f36710f60587aa1132e77b765dff45667d
alessandropierini <99676715+alessandropierini@users.noreply.github.com>
2023-07-10 18:25:52 -0400

    inicio del componente

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,8 +1,16 @@
 import * as React from 'react';
 
 import './App.css'
+import sampleData from './assets/sampleData'
 
 const App = () => {
+
+  React.useEffect(() => {
+    console.log(sampleData)
+  }) 
+
+  const data = sampleData
+  const keys = Object.keys(data.length ? data[0] : { });
 
   const options = [
     { label: 'Tabla de empleados', value: '1' },
@@ -11,41 +19,87 @@ const App = () => {
   ];
 
   const [value, setValue] = React.useState();
-
   const handleChange = (event) => {
     setValue(event.target.value);
   };
 
   return (
+    <>
+      <div style={{
+        padding: 50,
+        paddingLeft: 75,
+        paddingRight: 75,
+        backgroundColor: '#f5f5f5',
+        borderRadius: 15,
+        boxShadow: '1px 2px 25px gray',
+      }}>
 
-    <div style={{
-      padding: 50,
-      paddingLeft: 75,
-      paddingRight: 75,
-      backgroundColor: '#f5f5f5',
-      borderRadius: 15,
-      boxShadow: '1px 2px 25px gray',
-    }}>
-      <img
-        src='https://cdn-icons-png.flaticon.com/512/1157/1157109.png'
+        <img
+          src='https://cdn-icons-png.flaticon.com/512/1157/1157109.png'
+          style={{
+            height: '250px',
+          }} />
+
+        <h1>Generador de código</h1>
+        <p>Selecciona tabla deseada:</p>
+
+        <select value={value} onChange={handleChange}>
+          {options.map((option) => (<option value={option.value}>{option.label}</option>))}
+        </select>
+
+        {value && <p style={{ paddingTop: 35 }}>Se abrirá la tabla en una nueva ventana...</p>}
+
+      </div>
+
+      <div
+        class="container"
         style={{
-          height: '250px',
-          
-        }}
-      />
-      <h1>Generador de código</h1>
-      <p>
-        Selecciona tabla deseada:
-      </p>
+          padding: 25,
+          backgroundColor: '#f5f5f5',
+          borderRadius: 15,
+          boxShadow: '1px 2px 25px gray',
+        }}>
+        <div class="title">
+          <h3>Table title</h3>
+        </div>
+        <div class="buttons">
+          <button style={{margin: 2, width: '100%'}}>Guardar</button><br />
+          <button style={{margin: 2, width: '100%'}}>Eliminar</button><br />
+          <button style={{margin: 2, width: '100%'}}>Buscar</button><br />
+          <button style={{margin: 2, width: '100%'}}>Listar</button><br />
+        </div>
+        <div class="inputs">
+          <label style={{width: '20%'}}>Codigo: </label><input style={{width: '50%'}}></input><br />
+          <label style={{width: '20%'}}>Nombre: </label><input style={{width: '50%'}}></input>
 
-      <select value={value} onChange={handleChange}>
-        {options.map((option) => (
-          <option value={option.value}>{option.label}</option>
-        ))}
-      </select>
-      {value && <p style={{ paddingTop: 35 }}>Se abrirá la tabla en una nueva ventana...</p>}
-    </div>
+        </div>
+        <div class="table">
 
+            {sampleData.length > 0 && (
+              <table>
+                <thead>
+                  <tr>
+                    {keys.map((item, idx) => (
+                      <th key={idx}>{item}</th>
+                    ))}
+                  </tr>
+                </thead>
+                <tbody>
+                  {data.map((item, idx) => (
+                    <tr key={idx}>
+                      {keys.map((key, idx) => (
+                        <td>{item[key]}</td>
+                      ))}
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            )}
+            
+        </div>
+      </div>
+
+    </>
   );
 };
 
