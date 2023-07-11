139a8354b466155d40190a1286ac5bd6c6a7be0f
alessandropierini <99676715+alessandropierini@users.noreply.github.com>
2023-07-11 17:43:44 -0400

    Componente listo

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 
 import './App.css'
 import sampleData from './assets/sampleData'
+import TableView from './components/TableView';
 
 const App = () => {
 
@@ -10,7 +11,6 @@ const App = () => {
   }) 
 
   const data = sampleData
-  const keys = Object.keys(data.length ? data[0] : { });
 
   const options = [
     { label: 'Tabla de empleados', value: '1' },
@@ -51,53 +51,8 @@ const App = () => {
 
       </div>
 
-      <div
-        class="container"
-        style={{
-          padding: 25,
-          backgroundColor: '#f5f5f5',
-          borderRadius: 15,
-          boxShadow: '1px 2px 25px gray',
-        }}>
-        <div class="title">
-          <h3>Table title</h3>
-        </div>
-        <div class="buttons">
-          <button style={{margin: 2, width: '100%'}}>Guardar</button><br />
-          <button style={{margin: 2, width: '100%'}}>Eliminar</button><br />
-          <button style={{margin: 2, width: '100%'}}>Buscar</button><br />
-          <button style={{margin: 2, width: '100%'}}>Listar</button><br />
-        </div>
-        <div class="inputs">
-          <label style={{width: '20%'}}>Codigo: </label><input style={{width: '50%'}}></input><br />
-          <label style={{width: '20%'}}>Nombre: </label><input style={{width: '50%'}}></input>
-
-        </div>
-        <div class="table">
-
-            {sampleData.length > 0 && (
-              <table>
-                <thead>
-                  <tr>
-                    {keys.map((item, idx) => (
-                      <th key={idx}>{item}</th>
-                    ))}
-                  </tr>
-                </thead>
-                <tbody>
-                  {data.map((item, idx) => (
-                    <tr key={idx}>
-                      {keys.map((key, idx) => (
-                        <td>{item[key]}</td>
-                      ))}
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            )}
-            
-        </div>
-      </div>
+          <TableView sampleData={data} />
+      
 
     </>
   );
---
+++ react-project/src/components/TableView.jsx
@@ -0,0 +1,60 @@
+import '../App.css'
+import * as React from 'react';
+
+const TableView = ({ sampleData }) => {
+
+    const data = sampleData
+    const keys = Object.keys(data.length ? data[0] : {});
+
+    return (
+        <div
+            class="container"
+            style={{
+                padding: 25,
+                backgroundColor: '#f5f5f5',
+                borderRadius: 15,
+                boxShadow: '1px 2px 25px gray',
+            }}>
+            <div class="title">
+                <h3>Table title</h3>
+            </div>
+            <div class="buttons">
+                <button style={{ margin: 2, width: '100%' }}>Guardar</button><br />
+                <button style={{ margin: 2, width: '100%' }}>Eliminar</button><br />
+                <button style={{ margin: 2, width: '100%' }}>Buscar</button><br />
+                <button style={{ margin: 2, width: '100%' }}>Listar</button><br />
+            </div>
+            <div class="inputs">
+                <label style={{ width: '20%' }}>Codigo: </label><input style={{ width: '50%' }}></input><br />
+                <label style={{ width: '20%' }}>Nombre: </label><input style={{ width: '50%' }}></input>
+
+            </div>
+            <div class="table">
+
+                {sampleData.length > 0 && (
+                    <table>
+                        <thead>
+                            <tr>
+                                {keys.map((item, idx) => (
+                                    <th key={idx}>{item}</th>
+                                ))}
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {data.map((item, idx) => (
+                                <tr key={idx}>
+                                    {keys.map((key, idx) => (
+                                        <td>{item[key]}</td>
+                                    ))}
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                )}
+
+            </div>
+        </div>
+    )
+}
+
+export default TableView
