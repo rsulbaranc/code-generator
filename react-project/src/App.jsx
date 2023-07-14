66862f6ff7e16f4357286ea52b640d240f3085c6
alessandropierini <99676715+alessandropierini@users.noreply.github.com>
2023-07-13 21:17:41 -0400

    switch

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,21 +1,21 @@
-import * as React from 'react';
+import * as React from "react";
 
-import './App.css'
-import sampleData from './assets/sampleData'
-import TableView from './components/TableView';
+import "./App.css";
+import sampleData from "./assets/sampleData";
+
+import MyView from './MyView'
 
 const App = () => {
-
   React.useEffect(() => {
-    console.log(sampleData)
-  }) 
+    console.log(sampleData);
+  });
 
-  const data = sampleData
+  const data = sampleData;
 
   const options = [
-    { label: 'Tabla de empleados', value: '1' },
-    { label: 'Tabla de Productos', value: '2' },
-    { label: 'Tabla de administracion', value: '3' },
+    { label: "Tabla de empleados", value: "1" },
+    { label: "Tabla de Productos", value: "2" },
+    { label: "Tabla de administracion", value: "3" },
   ];
 
   const [value, setValue] = React.useState();
@@ -25,35 +25,38 @@ const App = () => {
 
   return (
     <>
-      <div style={{
-        padding: 50,
-        paddingLeft: 75,
-        paddingRight: 75,
-        backgroundColor: '#f5f5f5',
-        borderRadius: 15,
-        boxShadow: '1px 2px 25px gray',
-      }}>
-
+      { value ?  <div
+        style={{
+          padding: 50,
+          paddingLeft: 75,
+          paddingRight: 75,
+          backgroundColor: "#f5f5f5",
+          borderRadius: 15,
+          boxShadow: "1px 2px 25px gray",
+        }}
+      >
         <img
-          src='https://cdn-icons-png.flaticon.com/512/1157/1157109.png'
+          src="https://cdn-icons-png.flaticon.com/512/1157/1157109.png"
           style={{
-            height: '250px',
-          }} />
+            height: "250px",
+          }}
+        />
 
         <h1>Generador de código</h1>
         <p>Selecciona tabla deseada:</p>
 
         <select value={value} onChange={handleChange}>
-          {options.map((option) => (<option value={option.value}>{option.label}</option>))}
+          {options.map((option) => (
+            <option value={option.value}>{option.label}</option>
+          ))}
         </select>
 
-        {value && <p style={{ paddingTop: 35 }}>Se abrirá la tabla en una nueva ventana...</p>}
-
-      </div>
-
-          <TableView sampleData={data} />
-      
-
+        {value && (
+          <button role="link" onClick={() => openInNewTab({MyView})}>
+            Go
+          </button>
+        )}
+      </div> : <MyView/>}
     </>
   );
 };
